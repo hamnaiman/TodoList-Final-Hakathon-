dd24219aff4181d2ccba6dfcb1711150d6174933
hamna iman <hamnajawaid22@gmail.com>
2025-04-27 14:48:05 +0500

    added dist

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', 
-        changeOrigin: true,  
-        secure: false,
-      }
-    }
-    
-  },
   plugins: [react()],
-})
+  base: '/my-todo-app/',  // Replace 'my-todo-app' with your actual repo name
+});
